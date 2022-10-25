1b49ca81b772a10d3fc5f2123c7b61508d5838bd
etrushkina <trushkinaevg@gmail.com>
2022-10-25 20:19:57 +0300

    homework fragments part 1

---
+++ app/src/main/java/otus/gpb/homework/fragments/FragmentA.kt
@@ -0,0 +1,24 @@
+package otus.gpb.homework.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class FragmentA : Fragment(R.layout.fragment_a) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.btnOpenFragmentAA)
+        button.setOnClickListener {
+            val color = ColorGenerator.generateColor()
+            val fragment = FragmentAA()
+            val bundle = Bundle()
+            bundle.putInt(FragmentAA.BUNDLE_COLOR, color)
+            fragment.arguments = bundle
+            childFragmentManager.beginTransaction()
+                .replace(R.id.childFragConteiner, fragment)
+                .commit()
+        }
+    }
+}
--- app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
+++ app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
@@ -1,6 +1,8 @@
 package otus.gpb.homework.fragments
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btnOpenFragmentA)
+        button.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frgmtAContainer, FragmentA())
+                .commit()
+        }
     }
 }
