8ed1ca47fd550729d9e178191ffa094669074968
zanoza_room <etn88@yandex.ru>
2022-10-25 09:31:31 +0300

    Created main viewModel for save state for main fragment - FragmentA. This viewModel helps to understand when FragmentA must show main button for start his child fragment - FragmentAA

---
+++ app/src/main/java/otus/gpb/homework/fragments/MainViewModel.kt
@@ -0,0 +1,16 @@
+package otus.gpb.homework.fragments
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    private val _isShow = MutableLiveData(true)
+    var isShow:LiveData<Boolean> =_isShow
+
+    fun mainIsShow(boolean: Boolean){
+        _isShow.value = boolean
+        isShow =_isShow
+
+    }
+}
